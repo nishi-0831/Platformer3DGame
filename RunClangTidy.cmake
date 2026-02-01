bddb63b8e67fc5ad8c63ab0c23485ec0c4d19b4a
西川和樹 <190053668+nishi-0831@users.noreply.github.com>
2026-02-01 22:54:24 +0900

    Power Shell スクリプトでclang-tidy,clang-tidy-sarifを実行するよう変更した。cmakeスクリプトで変数を設定して呼び出す。 cmakeスクリプトでPower Shellを実行すると割愛するが上手くいかない

--- RunClangTidy.cmake
+++ RunClangTidy.cmake
@@ -4,18 +4,18 @@ set(EXE_PATH "$<SHELL_PATH:${CMAKE_CURRENT_SOURCE_DIR}/Tools/RunClangTidyWrapper
 set(SARIF_CONVERTER "$<SHELL_PATH:${CMAKE_CURRENT_SOURCE_DIR}/clang-tidy-sarif.exe>")
 set(OUTPUT_JSON "$<SHELL_PATH:${CMAKE_CURRENT_SOURCE_DIR}/tidy-output.json>")
 set(OUTPUT_SARIF "$<SHELL_PATH:${CMAKE_CURRENT_SOURCE_DIR}/tidy-output.sarif>")
+set(SCRIPT_FILE "${CMAKE_CURRENT_SOURCE_DIR}/RunClangTidy.ps1")
 
 # 呼び出し側で登録する
 function(register_clang_tidy_target FUNC_NAME)
-    # PowerShell 経由で実行してパイプで変換し、UTF-8で出力する
     add_custom_target(${FUNC_NAME}
-    # powershellでPowerShellが使えるようになる
-    # -NoProfileでプロファイルを読み込まない
-      COMMAND powershell "-NoProfile" "${EXE_PATH}" "${CMAKE_CURRENT_SOURCE_DIR}" "-p" "${CMAKE_CURRENT_BINARY_DIR}" "--" ">" "${OUTPUT_JSON}"
-      COMMAND powershell "-NoProfile" "Get-Content" "${OUTPUT_JSON}" "|" "${SARIF_CONVERTER}" "|" "Out-File" "${OUTPUT_SARIF}"
-      WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-      COMMENT "Run clang-tidy and convert to SARIF"
-      USES_TERMINAL
+        COMMAND powershell -NoProfile -ExecutionPolicy Bypass -File "${SCRIPT_FILE}"
+        -ClangTidyWrapper ${EXE_PATH} -SarifConverter ${SARIF_CONVERTER}
+        -OutputJson ${OUTPUT_JSON} -OutputSarif ${OUTPUT_SARIF}
+        -SrcDir ${CMAKE_SOURCE_DIR} -BinDir ${CMAKE_BINARY_DIR}
+        WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+        COMMENT "Run clang-tidy and convert to SARIF"
+        USES_TERMINAL
     )
 endfunction()
 
---
+++ RunClangTidy.ps1
@@ -0,0 +1,36 @@
+﻿Param(
+    [string]$ClangTidyWrapper,
+    [string]$SarifConverter,
+    [string]$OutputJson,
+    [string]$OutputSarif,
+    [string]$SrcDir,
+    [string]$BinDir
+    )
+
+function RunClangTidy
+{
+    # clang-tidyを起動し、出力をJSONファイルにリダイレクト
+    & $ClangTidyWrapper $SrcDir -p $BinDir -- | Out-File -FilePath $OutputJson -Encoding utf8
+    return 0
+}
+
+function ConvertToSarif
+{
+    # JSONファイルの内容をConverterに渡して、SARIFに変換
+    try 
+    {
+        Get-Content $OutputJson | & $SarifConverter | Out-File -FilePath $OutputSarif -Encoding utf8
+        
+    }
+    catch 
+    {
+        Write-Host "Error: $($_.Exception.Message)"
+        return 1;
+    }
+    return 0
+}
+
+RunClangTidy
+ConvertToSarif
+
+exit
