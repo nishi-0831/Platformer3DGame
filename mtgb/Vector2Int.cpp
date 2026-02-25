5cfa0491db9d291e43f92122c6ee62828a74b18a
西川和樹 <190053668+nishi-0831@users.noreply.github.com>
2026-02-25 10:41:40 +0900

    未使用のファイルを削除

--- mtgb/Vector2Int.cpp
+++ mtgb/Vector2Int.cpp
@@ -1,12 +1,1 @@
 #include "Vector2Int.h"
-
-#pragma region キャスト
-// Vector2Int::operator Vector2() const noexcept
-//{
-//	return Vector2
-//	{
-//		static_cast<float>(x),
-//		static_cast<float>(y)
-//	};
-// }
-#pragma endregion
